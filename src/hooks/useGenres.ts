3343211b03d048a9407b977b251948e349245de0
Cody <Cody@gmail.com>
2024-12-26 01:14:44 -0800

    Display Genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,6 +3,7 @@ import useData from "./useData-DESKTOP-0CFRKIJ";
 interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const useGenres = () => useData<Genre>('/genres');
